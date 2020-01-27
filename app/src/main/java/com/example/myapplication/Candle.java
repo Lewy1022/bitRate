49b302703e1242fcd108af87e912fc740721d39b
Andrzej Lewiński <andrzejlewy1022@gmail.com>
2020-01-27 21:03:45 +0100

    NOWY trend

--- app/src/main/java/com/example/myapplication/Candle.java
+++ app/src/main/java/com/example/myapplication/Candle.java
@@ -1,4 +1,22 @@
 package com.example.myapplication;
 
 public class Candle {
+
+    public Candle(String time, double low, double high, double open, double close,double volume){
+        this.time = time;
+        this.open = open;
+        this.close = close;
+        this.high = high;
+        this.low = low;
+        this.volume = volume;
+
+    }
+
+    String time;
+    double open;
+    double close;
+    double high;
+    double low;
+    double volume;
+
 }
